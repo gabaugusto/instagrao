5762feec1d3ed34ef1a01884b0401560a5828754
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2026-03-30 17:24:37 -0300

    List Item

--- src/LateralEsquerda/LateralEsquerda.jsx
+++ src/LateralEsquerda/LateralEsquerda.jsx
@@ -1,15 +1,25 @@
 
+import ListItem from './ListItem'
+import './LateralEsquerda.css'
+
+// Importações de Imagem
+// var nomeDaVariavel = require('caminho/para/imagem.jpg')
+import instagram from '../assets/logo-pequeno.jpg'
+
+var x = 10
+var y = 20
+
 export default function LateralEsquerda() {
     return (
         <div className='LateralEsquerda'>
-            <p>Logo do Instagram</p>
+            <img src={instagram} alt='Logo do Instagram' className='logoInstagram' />
             <ul>
-                <li>Home</li>
-                <li>Search</li>
-                <li>Explore</li>
-                <li>Reels</li>
-                <li>Messages</li>
-                <li>Notifications</li>
+                
+                <ListItem text="Home" />
+                <ListItem  text="Search" />
+                <ListItem  text="Explore" />
+                <ListItem  text="Reels" />
+
             </ul>
         </div>
     )
---
+++ src/LateralEsquerda/ListItem.jsx
@@ -0,0 +1,3 @@
+export default function ListItem(props) {
+    return <li>Icone {props.text}</li>
+}
